8702a9a5c3524d210056954762f930caa1eb5e14
anasbn44 <banouni.anas0@gmail.com>
2023-05-13 14:14:15 +0100

    Initial commit

---
+++ customer-service/src/main/java/ma/enset/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,27 @@
+package ma.enset.customerservice;
+
+import ma.enset.customerservice.entities.Customer;
+import ma.enset.customerservice.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository) {
+        return args -> {
+            customerRepository.save(new Customer(null, "nom1", "nom1@gmail.com"));
+            customerRepository.save(new Customer(null, "nom2", "nom2@gmail.com"));
+            customerRepository.save(new Customer(null, "nom3", "nom3@gmail.com"));
+
+        };
+    }
+
+}
---
+++ inventory-service/src/main/java/ma/enset/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,25 @@
+package ma.enset.inventoryservice;
+
+import ma.enset.inventoryservice.entities.Product;
+import ma.enset.inventoryservice.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(ProductRepository productRepository){
+        return args -> {
+            productRepository.save(new Product(null, "Laptop", 12000.0, 5));
+            productRepository.save(new Product(null, "Phone", 1000.0, 15));
+            productRepository.save(new Product(null, "Printer", 200.0, 25));
+        };
+    }
+}
